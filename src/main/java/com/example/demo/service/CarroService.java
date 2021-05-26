c336699a3ac8cac7a554be628d9faac9276960c2
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-05-26 16:25:03 -0300

    Alterada interface CarroService e classe CarroServiceImpl

--- src/main/java/com/example/demo/service/CarroService.java
+++ src/main/java/com/example/demo/service/CarroService.java
@@ -1,11 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.model.Carro;
-import com.example.demo.model.TipoCarro;
-
-import org.springframework.stereotype.Service;
-
-public class CarroService {
+public interface CarroService {
 
 
 }
