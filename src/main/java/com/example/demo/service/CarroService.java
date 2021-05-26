4b8daa9cb4e3a9647b262dde4b05c703a4dde162
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-05-26 16:15:08 -0300

    Adicionados Package Service, classe CarroService, package Impl e classe CarroServiceImpl

---
+++ src/main/java/com/example/demo/service/CarroService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import com.example.demo.model.Carro;
+import com.example.demo.model.TipoCarro;
+
+import org.springframework.stereotype.Service;
+
+public class CarroService {
+
+
+}
---
+++ src/main/java/com/example/demo/service/impl/CarroServiceImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.model.Carro;
+import com.example.demo.model.TipoCarro;
+import com.example.demo.repository.CarroRepository;
+import com.example.demo.service.CarroService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+public class CarroServiceImpl {
+
+
+}
