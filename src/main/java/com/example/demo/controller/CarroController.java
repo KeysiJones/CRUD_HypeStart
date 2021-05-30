0488f7fa4f823e36f163f5fbc8fab60fe1b2cc7b
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2021-05-30 11:38:08 -0300

    alteraçao Controller com objeto Carro e retorno dos endpoints

--- src/main/java/com/example/demo/controller/CarroController.java
+++ src/main/java/com/example/demo/controller/CarroController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import com.example.demo.enums.TipoCarro;
+import com.example.demo.model.Carro;
+import com.example.demo.service.CarroService;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.util.List;
 
 import static com.example.demo.util.Constants.PAGINA_INICIAL;
 
 @RestController
 @RequestMapping("/")
-public class CarroController {
+public class CarroController{
+
+    private final CarroService carroService;
+
+    public CarroController(CarroService carroService) {
+        this.carroService = carroService;
+    }
 
     @GetMapping
     public Mono<String> homePage() {
@@ -16,27 +28,44 @@ public class CarroController {
     }
 
     @GetMapping("/api/carros")
-    public Flux<String> listaDeCarros() {
-        return Flux.just("Um Celtinha,", " Uma Range Rover,", " Uma Lamborghini e", " Uma mercedes Benz");
+    public Flux<List<Carro>> listaDeCarros() {
+        List<Carro> carros = List.of( //Arrays.asList com return Flux.fromIterable
+                new Carro(1L, "Mercedes","W12", TipoCarro.CORRIDA),
+                new Carro(2L, "Toyota", "Yaris", TipoCarro.SEDAN),
+                new Carro(3L, "Ford","EcoSport", TipoCarro.SPORT));
+        return Flux.just(carros);
+        //return clienteService.listAllCars();
+
     }
 
-    @GetMapping("/api/carro/{id}")
-    public Mono<String> buscarCarroPorId(@PathVariable(name = "id") Long id) {
-        return Mono.just("Encontramos o carro com o ID " + id);
+    @GetMapping("/api/carros/{id}")
+    public Mono<Carro> buscarCarroPorId(@PathVariable Long id) {
+        Carro carro = new Carro(1L, "Mercedes","W12", TipoCarro.CORRIDA);
+        return Mono.just(carro);
+        //return carroService.buscarCarroPorId();
     }
 
+    @ResponseStatus(value = HttpStatus.CREATED)
     @PostMapping("/api/cadastrar")
-    public Mono<String> cadastrar(@RequestBody String modelo) {
-        return Mono.just("Carro com o modelo " + modelo + " cadastrado com sucesso");
+    public Mono<Carro> cadastrar(@RequestBody Carro carro) {
+        carro = new Carro(1L, "Mercedes","W12", TipoCarro.CORRIDA);
+        return Mono.just(carro);
+        //return  Mono.just(carroService.saveCar(carro));
     }
 
+    @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @PutMapping("/api/atualizar/{id}")
-    public Mono<String> atualizarCadastro(@PathVariable String id) {
-        return Mono.just("Carro com o ID " + id + " atualizado com sucesso");
+    public Mono<Carro> atualizarCadastro(@PathVariable Long id, @RequestBody Carro carroASerAtualizado) {
+        carroASerAtualizado = new Carro(1L, "Mercedes", "AMG", TipoCarro.CORRIDA);
+        return Mono.just(carroASerAtualizado);
+        //carroASerAtualizado = carroService.atualizarCarroPorId(id);
+        //return Mono.just(carroService.updateCar(carroASerAtualizado));
     }
 
-    @DeleteMapping("/api/delete/{id}")
-    public Mono<String> deletar(@PathVariable String id) {
-        return Mono.just("Carro com o ID " + id + " deletado com sucesso");
-    }
+//    @ResponseStatus(value = HttpStatus.NO_CONTENT)
+//    @DeleteMapping("/api/delete/{id}")
+//    public Mono<Carro> deletar(@PathVariable Long id) {
+//        Carro carro = carro.getId();
+//        return Mono.just(carroService.deleteCardById(id);
+//    }
 }
