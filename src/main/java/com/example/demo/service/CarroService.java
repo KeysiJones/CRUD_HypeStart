5faddf1692054c59080e98781aae3af090098eab
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-05-31 09:02:22 -0300

    Criadas assinaturas dos métodos da Interface CarroService

--- src/main/java/com/example/demo/service/CarroService.java
+++ src/main/java/com/example/demo/service/CarroService.java
@@ -1,6 +1,20 @@
 package com.example.demo.service;
 
+import com.example.demo.model.Carro;
+
+import reactor.core.publisher.Mono;
+import reactor.core.publisher.Flux;
+
+import java.util.List;
+
 public interface CarroService {
 
+    Mono<Carro> saveCar(Carro carro);
+
+    Flux<List<Carro>> listAllCars();
+
+    Mono<Carro> updateCar(Long Id);
+
+    Mono<Carro> deleteCarById(Long id);
 
 }
