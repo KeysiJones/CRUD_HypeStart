d1f27ced4f70f3902a889b53b3db01ffee43e8a0
Keysi Jones <55116367+KeysiJones@users.noreply.github.com>
2021-06-01 15:38:16 -0300

    ALC-9 Ajuste nos endpoints, implementação dos services, ajustes no ENUM para salvar corretamente no banco de dados, adicionada dependência para as validações, Adicionando teste unitário com Junit 5 e criação do Banner para os logs.

--- src/main/java/com/example/demo/service/impl/CarroServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/CarroServiceImpl.java
@@ -1,8 +1,67 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.model.Carro;
+import com.example.demo.repository.CarroRepository;
 import com.example.demo.service.CarroService;
+import com.example.demo.util.MyCustomHttpResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
+import java.util.Optional;
+
+@Service
 public class CarroServiceImpl implements CarroService {
 
+    @Autowired
+    CarroRepository repository;
 
+    @Override
+    public Mono<Carro> saveCar(Carro carro) {
+        Carro savedCar = repository.save(carro);
+        return Mono.just(savedCar);
+    }
+
+    @Override
+    public Flux<Carro> listAllCars() {
+        return Flux.fromIterable(repository.findAll());
+    }
+
+    @Override
+    public ResponseEntity<MyCustomHttpResponse> updateCar(Long id, Carro carro) {
+
+        return repository.findById(id).map(car -> {
+            car.setMarca(Optional.ofNullable(carro.getMarca()).orElse(car.getMarca()));
+            car.setModelo(Optional.ofNullable(carro.getModelo()).orElse(car.getModelo()));
+            car.setTipo(Optional.ofNullable(carro.getTipo()).orElse(car.getTipo()));
+            repository.save(car);
+
+            return new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.OK.value(), "Registro com ID " + id + " atualizado com sucesso"), HttpStatus.OK);
+        }).orElse(new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Nenhum registro com o ID " + id + " foi encontrado"), HttpStatus.NOT_FOUND));
+    }
+
+    @Override
+    public ResponseEntity<MyCustomHttpResponse> deleteCarById(Long id) {
+        if (repository.findById(id).isPresent()) {
+            repository.deleteById(id);
+            return new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.OK.value(), "Registro com o ID " + id + " deletado com sucesso"), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Registro com o ID " + id + " não foi encontrado"), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    public ResponseEntity<?> findCarById(Long id) {
+
+        Optional<Carro> carro = repository.findById(id);
+
+        if (carro.isPresent()) {
+            return new ResponseEntity<>(carro.get(), HttpStatus.OK);
+        }
+
+        return new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Nenhum carro com o ID " + id + " foi encontrado"), HttpStatus.NOT_FOUND);
+    }
 }
