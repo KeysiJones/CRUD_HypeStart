0c2a300ff8d934d94d43c185bc571883997fb4ab
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2021-06-04 15:59:23 -0300

    alc-12 - Criação de Testes para o endpoint buscarCarroPorTipo e Criação Classe CarroStub em Testes

--- src/test/java/com/example/demo/controller/CarroControllerTest.java
+++ src/test/java/com/example/demo/controller/CarroControllerTest.java
@@ -1,8 +1,9 @@
 package com.example.demo.controller;
-
 import com.example.demo.enums.TipoCarro;
 import com.example.demo.model.Carro;
 import com.example.demo.service.CarroService;
+import com.example.demo.stub.CarroStub;
+import com.example.demo.util.MyCustomHttpResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,10 +11,9 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
@@ -28,7 +28,7 @@ public class CarroControllerTest {
 
     @Test
     public void listAllCarsShouldReturnSuccessfulResponse() {
-        Carro carroStub = new Carro("LAMBORGHINI", "ALC-9000", TipoCarro.CORRIDA);
+        Carro carroStub = CarroStub.create(TipoCarro.SEDAN);
         Flux<Carro> expectedResponse = Flux.just(carroStub, carroStub);
         when(service.listAllCars()).thenReturn(expectedResponse);
         Flux<Carro> response = controller.listaDeCarros();
@@ -44,7 +44,7 @@ public class CarroControllerTest {
 
     @Test
     public void buscaCarroPorId() {
-        Carro carroStub = new Carro("FERRARI", "SPIDER", TipoCarro.SPORT);
+        Carro carroStub = CarroStub.create(TipoCarro.SPORT);
         ResponseEntity expectedResponse = new ResponseEntity<>(carroStub, HttpStatus.OK);
         when(service.findCarById(Long.valueOf("1"))).thenReturn(expectedResponse);
         ResponseEntity response = controller.buscarCarroPorId(Long.valueOf("1"));
@@ -52,6 +52,35 @@ public class CarroControllerTest {
                 () -> assertNotNull(response),
                 () -> assertTrue(response.hasBody()),
                 () -> assertEquals(expectedResponse.getBody(), response.getBody()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void buscarCarroPorTipoShouldReturnSuccessfulResponse() {
+        Carro carro = CarroStub.create(TipoCarro.SPORT);
+        List<Carro> carroStub = new ArrayList<>();
+        carroStub.add(carro);
+        ResponseEntity expectedResponse = new ResponseEntity<>(carroStub, HttpStatus.OK);
+        when(service.findAllCarsByTipo(TipoCarro.SPORT)).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorTipo(TipoCarro.SPORT);
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getBody(), response.getBody()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+                );
+    }
+
+    @Test
+    public void buscarCarroPorTipoShouldReturnMyCustomResponse() {
+        ResponseEntity expectedResponse = new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Nenhum carro com o Tipo" + TipoCarro.SPORT + "foi encontrado"), HttpStatus.NOT_FOUND);
+        when(service.findAllCarsByTipo(TipoCarro.SPORT)).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorTipo(TipoCarro.SPORT);
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getStatusCodeValue(), response.getStatusCodeValue()),
                 () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
         );
     }
