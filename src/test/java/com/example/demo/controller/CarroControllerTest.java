d98d6d865061a1df42b6bb75bba9ecdf7c6ac9e9
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2021-06-11 11:15:11 -0300

    Adicionado testes nos demais endpoints

--- src/test/java/com/example/demo/controller/CarroControllerTest.java
+++ src/test/java/com/example/demo/controller/CarroControllerTest.java
@@ -1,9 +1,9 @@
 package com.example.demo.controller;
+
 import com.example.demo.enums.TipoCarro;
 import com.example.demo.model.Carro;
 import com.example.demo.service.CarroService;
 import com.example.demo.stub.CarroStub;
-import com.example.demo.util.MyCustomHttpResponse;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -12,6 +12,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 import java.util.ArrayList;
 import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
@@ -72,14 +73,111 @@ public class CarroControllerTest {
     }
 
     @Test
-    public void buscarCarroPorTipoShouldReturnMyCustomResponse() {
-        ResponseEntity expectedResponse = new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Nenhum carro com o Tipo" + TipoCarro.SPORT + "foi encontrado"), HttpStatus.NOT_FOUND);
+    public void buscarCarroPorTipoShouldReturnNotFound() {
+        ResponseEntity expectedResponse = CarroStub.carroNotFound();
         when(service.findAllCarsByTipo(TipoCarro.SPORT)).thenReturn(expectedResponse);
         ResponseEntity response = controller.buscarCarroPorTipo(TipoCarro.SPORT);
         assertAll(
                 () -> assertNotNull(response),
                 () -> assertTrue(response.hasBody()),
                 () -> assertEquals(expectedResponse.getStatusCodeValue(), response.getStatusCodeValue()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void buscarCarroPorMarcaShouldReturnSuccessfulResponse() {
+        Carro carro = CarroStub.carroBasico();
+        List<Carro> carroStub = new ArrayList<>();
+        carroStub.add(carro);
+        ResponseEntity expectedResponse = new ResponseEntity<>(carroStub, HttpStatus.OK);
+        when(service.findAllCarsByMarca("FERRARI")).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorMarca("FERRARI");
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getBody(), response.getBody()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void buscarCarroPorMarcaShouldReturnNotFound() {
+        //ResponseEntity expectedResponse = new ResponseEntity<>(new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value(), "Nenhum carro com o Tipo" + TipoCarro.SPORT + "foi encontrado"), HttpStatus.NOT_FOUND);
+        ResponseEntity expectedResponse = CarroStub.carroNotFound();
+        when(service.findAllCarsByMarca("FERRARI")).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorMarca("FERRARI");
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getStatusCodeValue(), response.getStatusCodeValue()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void buscarCarroPorModeloShouldReturnSuccessfulResponse() {
+        Carro carro = CarroStub.carroBasico();
+        List<Carro> carroStub = new ArrayList<>();
+        carroStub.add(carro);
+        ResponseEntity expectedResponse = new ResponseEntity<>(carroStub, HttpStatus.OK);
+        when(service.findAllCarsByModelo("SPIDER")).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorModelo("SPIDER");
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getBody(), response.getBody()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void buscarCarroPorModeloShouldReturnNotFound() {
+        ResponseEntity expectedResponse = CarroStub.carroNotFound();
+        when(service.findAllCarsByModelo("SPIDER")).thenReturn(expectedResponse);
+        ResponseEntity response = controller.buscarCarroPorModelo("SPIDER");
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertTrue(response.hasBody()),
+                () -> assertEquals(expectedResponse.getStatusCodeValue(), response.getStatusCodeValue()),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void cadastrarCarroShouldReturnSuccessfulResponse() {
+        Carro carroStub = CarroStub.create(TipoCarro.SEDAN);
+        Mono<Carro> expectedResponse = Mono.just(carroStub);
+        when(service.saveCar(carroStub)).thenReturn(expectedResponse);
+        Mono<Carro> response = controller.cadastrar(carroStub);
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertEquals(expectedResponse.block().getTipo(), response.block().getTipo()),
+                () -> assertEquals(expectedResponse.block().getId(), response.block().getId()),
+                () -> assertEquals(expectedResponse.block().getModelo(), response.block().getModelo()),
+                () -> assertEquals(expectedResponse.block().getMarca(), response.block().getMarca())
+                );
+    }
+
+    @Test
+    public void atualizarCarroShouldReturnSuccessfulResponse() {
+        Carro updateCar = new Carro("Fiat", "Mobi", TipoCarro.HATCH);
+        ResponseEntity expectedResponse = new ResponseEntity(HttpStatus.OK);
+        when(service.updateCar(1L, updateCar)).thenReturn(expectedResponse);
+        ResponseEntity response = controller.atualizarCadastro(1L, updateCar);
+        assertAll(
+                () -> assertNotNull(response),
+                () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
+        );
+    }
+
+    @Test
+    public void deletarCarroShouldReturnSuccessfulResponse() {
+        ResponseEntity expectedResponse = new ResponseEntity(HttpStatus.OK);
+        when(service.deleteCarById(1L)).thenReturn(expectedResponse);
+        ResponseEntity response = controller.deletar(1L);
+        assertAll(
+                () -> assertNotNull(response),
                 () -> assertEquals(expectedResponse.getStatusCode(), response.getStatusCode())
         );
     }
--- src/test/java/com/example/demo/stub/CarroStub.java
+++ src/test/java/com/example/demo/stub/CarroStub.java
@@ -2,10 +2,25 @@ package com.example.demo.stub;
 
 import com.example.demo.enums.TipoCarro;
 import com.example.demo.model.Carro;
+import com.example.demo.util.MyCustomHttpResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 public class CarroStub {
 
     public static Carro create(TipoCarro tipoCarro){
         return new Carro ("FERRARI", "SPIDER", tipoCarro);
+    }
+
+    public static Carro carroBasico(){
+        return new Carro ("FERRARI", "SPIDER");
+    }
+
+    public static ResponseEntity carroNotFound(){
+        ResponseEntity expectedResponse = new ResponseEntity<>(
+                new MyCustomHttpResponse(HttpStatus.NOT_FOUND.value()
+                        , "Nenhum carro foi encontrado")
+                , HttpStatus.NOT_FOUND);
+        return expectedResponse;
     }
 }
