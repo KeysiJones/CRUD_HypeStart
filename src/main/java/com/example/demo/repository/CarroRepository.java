b1aca951fa1eb667a7872519b7826090f8c092eb
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-06-15 15:40:21 -0300

    add atributos "preco" e "quantidade" à Entidade Carro

--- src/main/java/com/example/demo/repository/CarroRepository.java
+++ src/main/java/com/example/demo/repository/CarroRepository.java
@@ -11,4 +11,6 @@ public interface CarroRepository extends JpaRepository<Carro, Long> {
     Optional<List<Carro>> findAllCarsByTipo(TipoCarro tipoCarro);
     Optional<List<Carro>> findAllCarsByMarcaIgnoreCaseContaining(String marca);
     Optional<List<Carro>> findAllCarsByModeloIgnoreCaseContaining(String modelo);
+    Optional<List<Carro>> findAllCarsByPreco(Double preco);
+    Optional<List<Carro>> findAllCarsByQuantidade(Integer quantidade);
 }
