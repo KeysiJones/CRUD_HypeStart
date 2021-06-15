d3cfb66acd9530b9dfcf4b8d2a501d6553b59843
Leonardo Marques Pereira das Neves <ledomarques@gmail.com>
2021-06-15 15:40:21 -0300

    add atributos "preco" e "quantidade" à Entidade Carro

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -1,2 +1,9 @@
 --DROP TABLE IF EXISTS carro;
-CREATE TABLE IF NOT EXISTS carro (id serial PRIMARY KEY, marca VARCHAR(255), modelo VARCHAR(255), tipo VARCHAR(255));
+CREATE TABLE IF NOT EXISTS carro (
+    id serial PRIMARY KEY,
+    marca VARCHAR(255),
+    modelo VARCHAR(255),
+    tipo VARCHAR(255),
+    preco REAL,
+    quantidade INTEGER
+);
