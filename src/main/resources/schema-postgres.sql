6af149374bf24e075115f5c6d2d0645c43b1ec36
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2021-06-18 11:58:53 -0300

    add entidade Cliente, ajuste de Relacionamento das entidades e ajuste no script sql,

--- src/main/resources/schema-postgres.sql
+++ src/main/resources/schema-postgres.sql
@@ -7,3 +7,19 @@ CREATE TABLE IF NOT EXISTS carro (
     preco REAL,
     quantidade INTEGER
 );
+
+CREATE TABLE IF NOT EXISTS pedido (
+    id serial PRIMARY KEY,
+    listaCarros VARCHAR(255),
+    valorTotal REAL,
+    formaPagamento VARCHAR(255)
+);
+
+CREATE TABLE IF NOT EXISTS cliente (
+    id serial PRIMARY KEY,
+    nome VARCHAR(255),
+    cpf VARCHAR(255),
+    email VARCHAR(255),
+    telefone VARCHAR(255),
+    carrosCliente VARCHAR(255)
+);
