b5605dbfedb5652894a1232d32644f7f99b42ffb
juliana.blicharz <juliana.blicharz@hypeflame.com.br>
2021-07-02 17:47:42 -0300

    add Swagger implementation

--- src/main/java/com/example/demo/controller/CarroController.java
+++ src/main/java/com/example/demo/controller/CarroController.java
@@ -4,6 +4,7 @@ import com.example.demo.enums.TipoCarro;
 import com.example.demo.model.Carro;
 import com.example.demo.service.CarroService;
 import com.example.demo.util.MyCustomHttpResponse;
+import io.swagger.annotations.ApiOperation;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -21,6 +22,8 @@ public class CarroController {
         this.service = service;
     }
 
+
+    @ApiOperation("List all available cars in database")
     @GetMapping("/api/list/cars")
     public Flux<Carro> listaDeCarros() {
         LOGGER.info("Iniciando consulta no banco de dados...");
@@ -28,54 +31,63 @@ public class CarroController {
                 .doOnNext(cars -> LOGGER.info("Retornando todos os carros cadastrados..."));
     }
 
+    @ApiOperation("Search a car by ID")
     @GetMapping("/api/search/car/{id}")
     public ResponseEntity<?> buscarCarroPorId(@PathVariable(name = "id") Long id) {
         LOGGER.info("Verificando se o carro com o ID {} existe", id);
         return service.findCarById(id);
     }
 
+    @ApiOperation("Search a car by type")
     @GetMapping("/api/search/cars/by_tipo")
     public ResponseEntity<?> buscarCarroPorTipo(@RequestParam TipoCarro tipoCarro) {
         LOGGER.info("Retornando todos os carros cadastrados do tipo {} ", tipoCarro);
         return service.findAllCarsByTipo(tipoCarro);
     }
 
+    @ApiOperation("Search a car by brand")
     @GetMapping("/api/search/cars/by_marca")
     public ResponseEntity<?> buscarCarroPorMarca(@RequestParam String marca) {
         LOGGER.info("Retornando todos os carros cadastrados da marca {} ", marca);
         return service.findAllCarsByMarca(marca);
     }
 
+    @ApiOperation("Search a car by model")
     @GetMapping("/api/search/cars/by_modelo")
     public ResponseEntity<?> buscarCarroPorModelo(@RequestParam String modelo) {
         LOGGER.info("Retornando todos os carros cadastrados do modelo {} .", modelo);
         return service.findAllCarsByModelo(modelo);
     }
 
+    @ApiOperation("Search a car by manufacturing year")
     @GetMapping("/api/search/cars/by_anoFabricacao")
     public ResponseEntity<?> buscarCarroPorAnoFabricacao(@RequestParam Integer anoFabricacao) {
         LOGGER.info("Retornando todos os carros fabricados em {} .", anoFabricacao);
         return service.findAllCarsByAnoFabricacao(anoFabricacao);
     }
 
+    @ApiOperation("Search a car by model year")
     @GetMapping("/api/search/cars/by_anoModelo")
     public ResponseEntity<?> buscarCarroPorAnoModelo(@RequestParam Integer anoModelo) {
         LOGGER.info("Retornando todos os carros ano/modelo {} .", anoModelo);
         return service.findAllCarsByAnoModelo(anoModelo);
     }
 
+    @ApiOperation("Search a car by price")
     @GetMapping("/api/search/cars/by_preco")
     public ResponseEntity<?> buscarCarroPorPreco(@RequestParam Double preco) {
         LOGGER.info("Retornando todos os carros cadastrados com o valor de {} Reais.", preco);
         return service.findAllCarsByPreco(preco);
     }
 
+    @ApiOperation("Search a car by model quantity")
     @GetMapping("/api/search/cars/by_qtd")
     public ResponseEntity<?> buscarCarroPorQuantidade(@RequestParam Integer quantidade) {
         LOGGER.info("Retornando todos os carros com {} itens em estoque.", quantidade);
         return service.findAllCarsByQuantidade(quantidade);
     }
 
+    @ApiOperation("Create a new car")
     @PostMapping("/api/create/car")
     public Mono<Carro> cadastrar(@RequestBody Carro carro) {
         LOGGER.info("Analisando dados enviados...");
@@ -83,12 +95,14 @@ public class CarroController {
                 .doOnNext(car -> LOGGER.info("Salvando um carro da {} ", carro.getMarca()));
     }
 
+    @ApiOperation("Update a car registration")
     @PutMapping("/api/update/car/{id}")
     public ResponseEntity<MyCustomHttpResponse> atualizarCadastro(@PathVariable Long id, @Valid @RequestBody Carro carro) {
         LOGGER.info("Verificando se o carro com o ID {} existe", id);
         return service.updateCar(id, carro);
     }
 
+    @ApiOperation("Delete a car by ID")
     @DeleteMapping("/api/delete/car/{id}")
     public ResponseEntity<MyCustomHttpResponse> deletar(@PathVariable Long id) {
         LOGGER.info("Verificando se o carro com o ID {} existe", id);
